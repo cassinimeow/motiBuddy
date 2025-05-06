35c5e4cfd9f7928aa9a01f7811339227d039ab8a
rycue <151157051+rycue@users.noreply.github.com>
2025-05-06 11:45:42 +0800

    WIP: Experimenting

--- app/src/main/java/com/pomobuddy/app/MainActivity.kt
+++ app/src/main/java/com/pomobuddy/app/MainActivity.kt
@@ -4,13 +4,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.pomobuddy.app.ui.theme.PomoBuddyTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +36,47 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PomoBuddyTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
+                var count by remember {
+                    mutableIntStateOf(0)
                 }
+
+                // WE ARE CREATING HISTORY!
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = count.toString(), fontSize = 60.sp)
+                    Button(onClick = {
+                        count++
+                    }) {
+                        Text(text = "Add")
+                    }
+                }
+
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun ButtonAdd() {
+    Icon(
+        imageVector = Icons.Default.Add, contentDescription = null, modifier = Modifier.size(100.dp)
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+@Preview(showBackground = true)
+fun DefaultPreview() {
     PomoBuddyTheme {
-        Greeting("Android")
+        LazyColumn(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            items(10) { i ->
+                ButtonAdd()
+            }
+        }
     }
 }
